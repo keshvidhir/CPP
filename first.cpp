a37e4db23745253cd45e246b2e1ee5879c1521bc
Aarushi2021 <aarushi0015.be21@chitkara.edu.in>
2022-03-28 12:30:20 +0530

    first commit

---
+++ first.cpp
@@ -0,0 +1,16 @@
+//to get factorial of a no.
+
+#include <iostream>
+using namespace std;
+int main(){
+		int n,x=1;
+			cin>>n;
+				for(int i=1;i<=n;i++)
+						{x= x*i;
+								}
+					cout<<x<<endl;
+						cout<<n;
+							return 0;
+								
+
+}
